5ce4979cbff1f08a746332c8a3028f744d6205e6
DerRiesenPanda <105128555+DerRiesenPanda@users.noreply.github.com>
2024-06-03 22:34:45 +0200

    Add files via upload

--- con_grad.R
+++ con_grad.R
@@ -1,14 +1,14 @@
 con_grad <- function(A,b,x){
   r <- b - A %*% x
   p <- r
-  j <- 1
+  j <- 0
   
   conv = list()
   err = list()
   conv[1] <- norm(r, type = '2')
   
   if(all(b == 0)){
-    err[1] <- norm(x, type = 'F')
+    err[1] <- norm(x, type = 'I')
   }
   
   while((norm(r, type = '2')/norm(b, type = '2') > 1.e-14) & j < 500){
@@ -22,19 +22,23 @@ con_grad <- function(A,b,x){
     conv[j] <- norm(r, '2')/norm(b, '2')
     
     if(all(b == 0)){
-      err[j] <- norm(x, type = 'F')
+      err[j] <- norm(x, type = 'I')
     }
   }
-  print(x)
-  print(j)
-  print(conv[j])
+  cat(sprintf('%5.0f %20.16e\n', j, conv[j]))
   
-  par(mfrow = c(1, 2))
-  plot(c(1:j), conv, type = 'l', xlab = 'iteration')
-  plot(c(1:j), conv, log = 'y', type ='l', xlab = 'iteration')
+  par(mfrow = c(2, 1))
   
+  plot(1:j, conv, type = "o", col = "blue", pch = 16, lty = 2, xlab = "Iteration", ylab = "Convergence")
   
+  positive_indices <- which(conv > 0)
+  plot(positive_indices, conv[positive_indices], type = "o", log = "y", pch = 16, lty = 2, col = "red", 
+       xlab = "Iteration", ylab = "Log Convergence")
   
+  # Return the convergence history and the final solution vector
+  return(list(conv = conv, x = x))
+  
+
   
 }
 
@@ -48,25 +52,26 @@ x_zero
 
 con_grad(A,b,x_zero)
 
-pre_con_grad <- function(A,b,x, M,tol){
+
+pre_con_grad <- function(A,b,x,M,tol){
   r <- b - A %*% x
-  z <- M/r
+  z <- solve(M, r)
   p <- z
   j <- 1
   
   conv = list()
   err = list()
-  conv[1] <- norm(r, type = '2')
+  conv[1] <- norm(r, type = '2') 
   
   if(all(b == 0)){
-    err[1] <- norm(x, type = 'F')
+    err[1] <- norm(x, type = 'I')
   }
   
-  while((norm(r, type = '2')/norm(b, type = '2') > tol) & j < 500){
-    alpha <- (t(r) %*% r)/(t(p) %*% A %*% p)
+  while((norm(r, type = '2')/norm(b, type = '2') > tol) && j < 500){
+    alpha <- (t(z) %*% r)/(t(p) %*% A %*% p)
     x <- x + as.vector(alpha) * p
     rnew <- r - as.vector(alpha) * A %*% p
-    znew <- M/rnew
+    znew <- solve(M, rnew)
     beta <- (t(znew) %*% rnew)/(t(z) %*% r)
     p <- znew + as.vector(beta) * p
     r <- rnew
@@ -75,17 +80,20 @@ pre_con_grad <- function(A,b,x, M,tol){
     conv[j] <- norm(r, '2')/norm(b, '2')
     
     if(all(b == 0)){
-      err[j] <- norm(x, type = 'F')
+      err[j] <- norm(x, type = 'I')
     }
   }
-  print(x)
-  print(j)
-  print(conv[j])
+  cat(sprintf('%5.0f %20.16e\n', j, conv[j]))
   
-  par(mfrow = c(1, 2))
-  plot(c(1:j), conv, type = 'l', xlab = 'iteration')
-  plot(c(1:j), conv, log = 'y', type ='l', xlab = 'iteration')
+  plot(1:j, conv, type = "o", col = "blue", pch = 16, lty = 2, xlab = "Iteration", ylab = "Convergence")
+  plot(1:j, conv, type = "o", col = "blue", pch = 16, lty = 2, log = "y", xlab = "Iteration", ylab = "Log Convergence")
+  
+  return(list(conv = conv, x = x))
   
 }
 
+M <- diag(c(4,3)) #Jacobi preconditioner 
+#https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.cse.psu.edu/~b58/cse456/lecture20.pdf&ved=2ahUKEwi99f28oMCGAxVo2gIHHT67AY4QFnoECBYQAQ&usg=AOvVaw1PLAHorVnnAUS7vzuSUq69
+M
 
+pre_con_grad(A,b,x_zero,M, 1.e-14)
