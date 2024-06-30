87475f5de231363515d99b568581b445ca2b2106
Paul Richard Jarschke <prj.nw@t-online.de>
2024-06-30 21:00:07 +0200

    Solved Ex1

--- Homework_2/HW2_Ex1.R
+++ Homework_2/HW2_Ex1.R
@@ -2,14 +2,14 @@
 
 # Set seed for reproducibility
 set.seed(1234)
-n <- 200    #much better results/estimation with 2000 or even 20000 (longer)
+n <- 200               # much better estimation with 2000 or even 20000 (longer)
 x <- runif(n, -5, 5)
 
 
 
 ############ (2) ############
 
-# Define the function to generate y
+# Define function to generate y
 generate_y <- function(x, beta, sigma, a, b, pi) {
   n <- length(x)
   y <- numeric(n)
@@ -23,17 +23,17 @@ generate_y <- function(x, beta, sigma, a, b, pi) {
   return(y)
 }
 
-# Define the parameters
+# Define parameters
 beta <- c(0, 1.5)
 sigma <- 1.0
 a <- 20
 b <- 20
 pi <- 0.45
 
-# Generate the response y using the defined function
+# Generate response y
 y <- generate_y(x, beta, sigma, a, b, pi)
 
-# Create a data frame with the generated data
+# Create data frame with generated data
 data <- data.frame(x = x, y = y)
 
 # Look at data
@@ -42,7 +42,7 @@ head(data)
 # Fit OLS regression model
 ols_model <- lm(y ~ x, data = data)
 
-# Plot the data and the regression line
+# Plot data and regression line
 plot(data$x, data$y, main = "OLS Regression", xlab = "x", ylab = "y", pch = 19, col = "blue")
 abline(ols_model, col = "red", lwd = 2)
 
@@ -57,12 +57,12 @@ em_algorithm <- function(data, beta, sigma, a, pi, max_iter = 100, tol = 1e-6) {
   y <- data$y
 
   for (iter in 1:max_iter) {
-    # E-step: Calculate the posterior probabilities
+    # E-step: Calculate posterior probabilities
     p1 <- pi * dnorm(y, mean = beta[1] + beta[2] * x, sd = sigma)
     p2 <- (1 - pi) * dunif(y, -a, b)
     w <- p1 / (p1 + p2)
 
-    # M-step: Update the parameters
+    # M-step: Update parameters
     lm_fit <- lm(y ~ x, weights = w)
     beta_new <- coef(lm_fit)
     sigma_new <- sqrt(sum(w * (y - predict(lm_fit))^2) / sum(w))
@@ -72,12 +72,12 @@ em_algorithm <- function(data, beta, sigma, a, pi, max_iter = 100, tol = 1e-6) {
     if (max(abs(beta_new - beta), abs(sigma_new - sigma), abs(pi_new - pi)) < tol) {
       break
     }
-
+    # Update variables
     beta <- beta_new
     sigma <- sigma_new
     pi <- pi_new
   }
-
+  # Create result list
   list(beta = beta, sigma = sigma, pi = pi, iter = iter)
 }
 
@@ -86,26 +86,6 @@ beta_init <- c(0, 1.5)
 sigma_init <- 1.0
 pi_init <- 0.45
 
-# Run the EM algorithm
+# Run EM algorithm
 em_result <- em_algorithm(data, beta_init, sigma_init, a, pi_init)
 em_result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
