01d9985d653484b4a4ca975b005a204c13fac0c3
leon-l-247 <leon.loeppert@hotmail.de>
2024-07-06 21:48:20 +0200

    omg - even more plots!

--- Homework_2/HW2_Ex2.R
+++ Homework_2/HW2_Ex2.R
@@ -13,7 +13,7 @@ set.seed(247)
 n_obs <- 50
 n_clus <- 5
 n_total <- n_obs * n_clus
-theta <- c(1, 2)
+theta <- c(1, 0.5)
 sigma_clus <- 1.5
 lambda <- 5
 cluster <- rep(1:n_clus, each=n_obs) # Cluster indicator
@@ -25,6 +25,14 @@ linear_predictor <- theta[1] + theta[2] * x + u[cluster]
 lambda <- exp(linear_predictor) # lambda is the mean (prediction) of each observation
 y <- rpois(n_total, lambda = lambda)
 data_glmm <- data.frame(y = y, x = x, cluster = as.factor(cluster))
+
+# Plot data ----
+data_plot <-
+  ggplot(data=data_glmm, aes(x, y, color = cluster)) +
+  geom_point(alpha=0.4) +
+  labs(title = "Data") +
+  theme(plot.title = element_text(hjust = 0.5))
+data_plot
 
 # 2.1) Use TMB to fit GLMM ----
 # install.packages("glmmTMB)
@@ -40,14 +48,18 @@ fixef(glmm_model) # Fixed Effects (FE)
 ranef(glmm_model) # Random Effects (RE)
 coef(glmm_model) # Total Effects (FE + RE)
 
-# Random Intercept + Slope
-glmm_model2 <-
-  glmmTMB(y ~ x + (x | cluster), family = poisson, data = data_glmm)
-summary(glmm_model2)
+# Plot fitted values
+data_plot +
+  geom_line(aes(x, fitted(glmm_model), color = cluster), linetype = "dashed")
 
-fixef(glmm_model2) # Fixed Effects (FE)
-ranef(glmm_model2) # Random Effects (RE)
-coef(glmm_model2) # Total Effects (FE + RE)
+# # Random Intercept + Slope (ignore)
+# glmm_model2 <-
+#   glmmTMB(y ~ x + (x | cluster), family = poisson, data = data_glmm)
+# summary(glmm_model2)
+# 
+# fixef(glmm_model2) # Fixed Effects (FE)
+# ranef(glmm_model2) # Random Effects (RE)
+# coef(glmm_model2) # Total Effects (FE + RE)
 
 # 2.2) Use R-INLA to fit GLMM ----
 library(INLA)
@@ -67,19 +79,22 @@ summary(inla_model)
 inla_model$summary.fixed # Fixed Effect distribution
 inla_model$summary.random # Random Effect distribution
 
-# Random Intercept + Slope (uncorrelated) 
-cluster2 <- cluster + 5
-formula2 <- y ~ x + f(cluster, model = "iid") + f(cluster2, x, model = "iid")
-inla_model2 <-
-  inla(
-    formula2,
-    family = "poisson",
-    data = data_glmm,
-    control.predictor = list(compute = TRUE),
-    control.compute=list(return.marginals.predictor=TRUE)
-  )
-summary(inla_model2)
+# # Random Intercept + Slope (uncorrelated) (ignore)
+# cluster2 <- cluster + 5
+# formula2 <- y ~ x + f(cluster, model = "iid") + f(cluster2, x, model = "iid")
+# inla_model2 <-
+#   inla(
+#     formula2,
+#     family = "poisson",
+#     data = data_glmm,
+#     control.predictor = list(compute = TRUE),
+#     control.compute=list(return.marginals.predictor=TRUE)
+#   )
+# summary(inla_model2)
 
+# Plot fitted values
+data_plot +
+  geom_line(aes(x, c(inla_model$summary.fitted.values["mean"])$mean, color = cluster), linetype = "dotted")
 
 # 2.3) Compare Results (for Random Intercept Model only) ----
 # Extract fixed effect estimates
@@ -99,8 +114,26 @@ print(inla_random)
 ci_glmm <- confint(glmm_model)
 print(ci_glmm)
 
-# 2.4) Comparative Plot ----
-# Plot fixed effect estimates
+# 2.4) Comparative Plots ----
+library(ggplot2)
+
+# Compare fitted values between TMB and INLA
+glmm_fitted <- data.frame(
+  x = x,
+  TMB = fitted(glmm_model),
+  INLA = c(inla_model$summary.fitted.values["mean"])$mean,
+  cluster = data_glmm$cluster
+)
+
+data_plot +
+  geom_line(data = glmm_fitted, aes(x = x, y = TMB, linetype = "TMB")) +
+  geom_line(data = glmm_fitted, aes(x = x, y = INLA, linetype = "INLA")) +
+  scale_linetype_manual(name = "Fit", values = c("dashed", "dotted"), 
+                        breaks = c("TMB", "INLA"), labels = c("TMB", "INLA")) +
+  labs(title = "Fitted Models Comparison", x = "X Axis", y = "Y Axis") 
+  # identical mean predictions
+
+# Compare fixed effect estimates
 fixed_effects <- data.frame(
   Model = c("glmmTMB", "glmmTMB", "R-INLA", "R-INLA"),
   Parameter = rep(c("Intercept", "Slope"), 2),
@@ -109,20 +142,31 @@ fixed_effects <- data.frame(
   Upper = c(ci_glmm[1, 2], ci_glmm[2, 2], inla_fixed$`0.975quant`[1], inla_fixed$`0.975quant`[2])
 )
 
-library(ggplot2)
 ggplot(fixed_effects, aes(x = Parameter, y = Estimate, color = Model)) +
   geom_point(position = position_dodge(width = 0.5)) +
   geom_errorbar(aes(ymin = Lower, ymax = Upper), width = 0.2, position = position_dodge(width = 0.5)) +
   labs(title = "Fixed Effect Estimates (+ 95% CI)", y = "Estimate")
 
-# Plot random effect estimates
-inla_random_df <- data.frame(ID = inla_random$ID, Mean = inla_random$mean, Lower = inla_random$`0.025quant`, Upper = inla_random$`0.975quant`)
-ggplot(inla_random_df, aes(x = ID, y = Mean)) +
-  geom_point() +
+# You can not compare random effects from TMB and R-INLA graphically in the same way as above,
+# since TMB does not provide estimates for uncertainty of Random Effects 
+# as it does for the fixed effects. 
+# R-INLA, however, does so:
+random_effects_df <-
+  data.frame(
+    cluster = inla_random$ID,
+    Mean_INLA = inla_random$mean,
+    Lower = inla_random$`0.025quant`,
+    Upper = inla_random$`0.975quant`,
+    Mean_TMB = c(glmm_random$cond$cluster)$`(Intercept)`
+  )
+ggplot(random_effects_df, aes(x = cluster, y = Mean_INLA)) +
+  geom_point(size=1.2) +
   geom_errorbar(aes(ymin = Lower, ymax = Upper), width = 0.2) +
-  labs(title = "Random Effect Estimates (R-INLA)", y = "Estimate")
+  labs(title = "Random Effect Estimates (R-INLA)", y = "Estimate") +
+  geom_point(aes(x=cluster, y = Mean_TMB), color = "pink", size = 0.8)
+  # this graph is not really pretty but you can see that the mean estimates are similar
 
-# Plot Posterior Distribution of R-Inla Parameter Estimates
+# Plot Posterior Distribution of R-INLA Parameter Estimates
 marginals_fixed <- inla_model$marginals.fixed
 marginals_random <- inla_model$marginals.random$cluster
 names(marginals_random) <- paste0("random_intercept.", 1:n_clus)
