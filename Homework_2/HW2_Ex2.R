5034da857be2061d881e953d80a120fd6bd7e052
leon-l-247 <leon.loeppert@hotmail.de>
2024-07-06 19:56:17 +0200

    ex3 draft (ignore 2.4)

--- Homework_2/HW2_Ex2.R
+++ Homework_2/HW2_Ex2.R
@@ -30,21 +30,66 @@ data_glmm <- data.frame(y = y, x = x, cluster = as.factor(cluster))
 # install.packages("glmmTMB)
 library(glmmTMB)
 
-# Random Intercept Model
-glmm_model <- glmmTMB(y ~ x + (1 | cluster), family = poisson, data = data_glmm)
-summary(glmm_model)
-coef(glmm_model)
-ranef(glmm_model)
 
-# Mixed Effects Model (Random Intercept + Slope)
-glmm_model2 <- glmmTMB(y ~ x + (x | cluster), family = poisson, data = data_glmm)
+# Random Intercept Model
+glmm_model <-
+  glmmTMB(y ~ x + (1 | cluster), family = poisson, data = data_glmm)
+summary(glmm_model)
+
+fixef(glmm_model) # Fixed Effects (FE)
+ranef(glmm_model) # Random Effects (RE)
+coef(glmm_model) # Total Effects (FE + RE)
+
+# Random Intercept + Slope
+glmm_model2 <-
+  glmmTMB(y ~ x + (x | cluster), family = poisson, data = data_glmm)
 summary(glmm_model2)
-coef(glmm_model2)
-ranef(glmm_model2)
+
+fixef(glmm_model2) # Fixed Effects (FE)
+ranef(glmm_model2) # Random Effects (RE)
+coef(glmm_model2) # Total Effects (FE + RE)
 
 # 2.2) Use R-INLA to fit GLMM ----
 library(INLA)
 
-# 2.3) Compare Results ----
+# Random Intercept Model
+formula <- y ~ x + f(cluster, model = "iid")
+inla_model <-
+  inla(
+    formula,
+    family = "poisson",
+    data = data_glmm
+  )
+summary(inla_model)
+
+inla_model$summary.fixed # Fixed Effect distribution
+inla_model$summary.random # Random Effect distribution
+
+# Random Intercept + Slope (uncorrelated) 
+cluster2 <- cluster + 5
+formula2 <- y ~ x + f(cluster, model = "iid") + f(cluster2, x, model = "iid")
+inla_model2 <-
+  inla(
+    formula2,
+    family = "poisson",
+    data = data_glmm
+  )
+summary(inla_model2)
+
+
+# 2.3) Compare Results (for Random Intercept Model only) ----
+# Extract fixed effect estimates
+glmm_fixed <- fixef(glmm_model)
+inla_fixed <- inla_model$summary.fixed
+
+# Extract random effect estimates
+glmm_random <- ranef(glmm_model)
+inla_random <- inla_model$summary.random$cluster
+
+# Print the estimates
+print(glmm_fixed)
+print(inla_fixed)
+print(glmm_random)
+print(inla_random)
 
 # 2.4) Comparative Plot ----
