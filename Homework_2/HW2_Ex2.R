3c63f71f37cfa5f136954580f5e76d5fb8993a00
leon-l-247 <leon.loeppert@hotmail.de>
2024-07-06 18:56:50 +0200

    first draft for glmmtmb

--- Homework_2/HW2_Ex2.R
+++ Homework_2/HW2_Ex2.R
@@ -9,19 +9,38 @@
 # ---------------------------------------------------------------
 
 # Initialization ----
+set.seed(247)
 n_obs <- 50
 n_clus <- 5
+n_total <- n_obs * n_clus
 theta <- c(1, 2)
-x <- rnorm(n_obs, mean = theta[1], sd = theta[2])
 sigma_clus <- 1.5
-cluster <- rnorm(n_clus, mean = 0, sd = sigma_clus)
 lambda <- 5
-y <- rpois(n_obs, lambda = lambda)
+cluster <- rep(1:n_clus, each=n_obs) # Cluster indicator
 
+# Simulate data ----
+x <- rnorm(n_total, 0, 1) # covariate
+u <- rnorm(n_clus, 0, sigma_clus) # random effect per cluster
+linear_predictor <- theta[1] + theta[2] * x + u[cluster]
+lambda <- exp(linear_predictor) # lambda is the mean (prediction) of each observation
+y <- rpois(n_total, lambda = lambda)
+data_glmm <- data.frame(y = y, x = x, cluster = as.factor(cluster))
 
 # 2.1) Use TMB to fit GLMM ----
 # install.packages("glmmTMB)
 library(glmmTMB)
+
+# Random Intercept Model
+glmm_model <- glmmTMB(y ~ x + (1 | cluster), family = poisson, data = data_glmm)
+summary(glmm_model)
+coef(glmm_model)
+ranef(glmm_model)
+
+# Mixed Effects Model (Random Intercept + Slope)
+glmm_model2 <- glmmTMB(y ~ x + (x | cluster), family = poisson, data = data_glmm)
+summary(glmm_model2)
+coef(glmm_model2)
+ranef(glmm_model2)
 
 # 2.2) Use R-INLA to fit GLMM ----
 library(INLA)
