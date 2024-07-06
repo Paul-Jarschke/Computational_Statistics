50682ed5f78499338ba77f4b9fe886b98a7ad3e3
leon-l-247 <leon.loeppert@hotmail.de>
2024-07-06 17:59:09 +0200

    moving files + finetuning HW2 - Ex1

--- Homework_2/HW2_Ex1.R
+++ Homework_2/HW2_Ex1.R
@@ -1,11 +1,33 @@
-############ (1) ############
+# --------------------------------------------------------------
+# Computational Statistics
+# Homework 2
+# Names: Paul Jarschke, Jan Parlesak, Leon Löppert
+# --------------------------------------------------------------
+
+# -----------------------------
+# Problem 1 - EM Algorithm ----
+# -----------------------------
+
+# 1.1) ----
 
 # Set seed for reproducibility
 set.seed(1234)
-n <- 200             # much better estimation with 2000 or even 20000 (longer)
+n <- 2000
+
+# Generate random uniform variable (covariate x)
 x <- runif(n, -5, 5)
 
-############ (2) ############
+# Initialize parameters
+beta <- c(0, 1.5)
+sigma <- 1.0
+a <- 20
+b <- a
+pi <- 0.45
+theta <- c(beta, sigma, pi)
+c <- 1/(2*a)  # uniform distribution is symmetric around zero
+
+
+# 1.2) ----
 
 # Define function to generate y
 generate_y <- function(x, beta, sigma, a, b, pi) {
@@ -21,54 +43,60 @@ generate_y <- function(x, beta, sigma, a, b, pi) {
   return(y)
 }
 
-# Define parameters
-beta <- c(0, 1.5)
-sigma <- 1.0
-a <- 20
-b <- 20
-pi <- 0.45
 
 # Generate response y
 y <- generate_y(x, beta, sigma, a, b, pi)
 
-# Create data frame with generated data
+# Store data in data frame
 data <- data.frame(x = x, y = y)
-
-# Look at data
 head(data)
 
 # Fit OLS regression model
 ols_model <- lm(y ~ x, data = data)
+coef(ols_model)
 
 # Plot data and regression line
-plot(data$x, data$y, main = "OLS Regression", xlab = "x", ylab = "y", pch = 19, col = "blue")
+plot(
+  data$x,
+  data$y,
+  main = "OLS Regression",
+  xlab = "x",
+  ylab = "y",
+  pch = 19,
+  col = "blue"
+)
 abline(ols_model, col = "red", lwd = 2)
-
+legend("topright", legend = c("Data", "Fit"), col = c("blue", "red"), 
+       pch = c(19, NA), lty = c(NA, 1), lwd = c(NA, 2))
 
 
 
 ############ (3) ############
 
-em_algorithm <- function(data, beta, sigma, a, pi, max_iter = 100, tol = 1e-6) {
+em_algorithm <- function(data, theta_init, a, max_iter = 100, tol = 1e-6) {
   n <- nrow(data)
   x <- data$x
   y <- data$y
+  
+  beta <- theta_init[[1]]
+  sigma <- theta_init[[2]]
+  pi <- theta_init[[3]]
 
   for (iter in 1:max_iter) {
     # E-step: Calculate posterior probabilities
     p1 <- pi * dnorm(y, mean = beta[1] + beta[2] * x, sd = sigma)
     p2 <- (1 - pi) * dunif(y, -a, b)
-    w <- p1 / (p1 + p2)
+    Ez <- p1 / (p1 + p2)
 
     # M-step: Update parameters
-    lm_fit <- lm(y ~ x, weights = w)
+    lm_fit <- lm(y ~ x, weights = Ez)
     beta_new <- coef(lm_fit)
-    sigma_new <- sqrt(sum(w * (y - predict(lm_fit))^2) / sum(w))
-    pi_new <- mean(w)
+    sigma_new <- sqrt(sum(Ez * (y - predict(lm_fit))^2) / sum(Ez))
+    pi_new <- mean(Ez)
 
     # Check for convergence
     if (max(abs(beta_new - beta), abs(sigma_new - sigma), abs(pi_new - pi)) < tol) {
-      break
+      break # break when all parameters are converged
     }
     # Update variables
     beta <- beta_new
@@ -80,5 +108,10 @@ em_algorithm <- function(data, beta, sigma, a, pi, max_iter = 100, tol = 1e-6) {
 }
 
 # Run EM algorithm
-em_result <- em_algorithm(data, beta, sigma, a, pi)
+beta_init <- c(0, 1)
+sigma_init <- 4
+pi_init <- 0.5
+theta_init <- list(beta_init, sigma_init, pi_init)
+
+em_result <- em_algorithm(data, theta_init, a)
 em_result
